adadf81574d258c90463e004286dae8e8e3381db
NabilaRahmaAulia <d1041231030@student.untan.ac.id>
2025-10-16 15:08:03 +0700

    Update db.php

--- db.php
+++ db.php
@@ -4,11 +4,10 @@ $username = "if0_40180786";
 $password = "kea9RNNfFU0jl"; 
 $database = "if0_40180786_sqlfacebookclone";
 
-// Create connection
 $conn = mysqli_connect($servername, $username, $password, $database);
 
-// Check connection
 if (!$conn) {
     die("Connection failed: " . mysqli_connect_error());
 }
 ?>
+
